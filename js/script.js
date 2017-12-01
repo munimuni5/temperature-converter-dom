b2a7cc23528d08312571166df4b258863682ff3e
munimuni5”git config --global user.name munimuni5” <emileeleamons@gmail.com>
2017-12-01 14:05:50 -0800

    Completed Temp Convert

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log('Hello, front end');
+console.log('This room is hot and cold');
+
+function convert(degree) {
+	var x;
+	if(degree == "C") {
+		x = document.getElementById("c").value * 9 / 5 + 32;
+			document.getElementById("f").value = Math.round(x);
+		} else {
+			x = (document.getElementById("f").value -32) * 5 /9;
+				 document.getElementById("c").value = Math.round(x);
+			}
+}
